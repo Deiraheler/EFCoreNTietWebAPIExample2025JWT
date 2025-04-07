81654802f96888b0e0e718b95cebab0f2e80e662
Deiraheler <53556704+Deiraheler@users.noreply.github.com>
2025-04-07 18:04:20 +0100

    Week 6 Lab Supplier View setup S00243021

--- RAD302Week6BlazorClient2025.S00243021/Pages/SupplierView.razor.cs
+++ RAD302Week6BlazorClient2025.S00243021/Pages/SupplierView.razor.cs
@@ -2,17 +2,55 @@ using Microsoft.AspNetCore.Components;
 using System.Net.Http;
 using System.Threading.Tasks;
 using RAD302Week6Lab2025.DataModel.S00243021;
+using RAD302Week6BlazorClient.S00243021;
+using System.Net.Http.Json;
 
 namespace RAD302Week6BlazorClient2025.S00243021.Pages
 {
     public partial class SupplierView
     {
+        private Supplier[] suppliers;
+        private Product[] products;
+
         [Inject]
         private HttpClient Http { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            // Load data using HttpClient
+            // Loading page for the first time
+            if (SupplierStaticContext.Suppliers == null)
+            {
+                SupplierStaticContext.Suppliers = await Http.GetFromJsonAsync<Supplier[]>("sample-data/Supplier.json");
+                SupplierStaticContext.Products = await Http.GetFromJsonAsync<Product[]>("sample-data/Product.json");
+                InitialiseSupplierProducts();
+            }
+            else
+            {
+                suppliers = SupplierStaticContext.Suppliers;
+            }
+            await base.OnInitializedAsync();
         }
+
+        private void InitialiseSupplierProducts()
+        {
+            if (suppliers != null && products != null)
+            {
+                foreach (var item in suppliers)
+                {
+                    List<Product> toadd = products
+                        .OrderBy(o => new Random().Next(2372))
+                        .Take(3)
+                        .ToList();
+
+                    item.SupplierProducts = new List<Product>();
+                    item.SupplierProducts.AddRange(toadd);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Suppliers or products are null.");
+            }
+        }
+
     }
 }
--- RAD302Week6BlazorClient2025.S00243021/Program.cs
+++ RAD302Week6BlazorClient2025.S00243021/Program.cs
@@ -7,8 +7,8 @@ namespace RAD302Week6BlazorClient2025.S00243021
     {
         public static async Task Main(string[] args)
         {
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
